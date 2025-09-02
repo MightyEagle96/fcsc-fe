a63164cb2c2bff133de5c98d9bdab9f58dc29482
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2025-09-02 07:03:42 +0100

    made changes

--- src/pages/ViewCandidates.jsx
+++ src/pages/ViewCandidates.jsx
@@ -4,8 +4,23 @@ import { CircularProgress } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
+  {
+    field: "id",
+    headerName: "S/N",
+    width: 90,
+    sortable: false,
+    filterable: false,
+    //renderCell: (params) => params.api.getRowIndex(params.id) + 1,
+  },
   { field: "ippisNumber", headerName: "IPPIS Number", width: 150 },
-  { field: "fullName", headerName: "Full Name", width: 200 },
+  {
+    field: "fullName",
+    headerName: "Full Name",
+    width: 200,
+    renderCell: (params) => (
+      <span>{params.value?.toUpperCase()}</span> // full uppercase
+    ),
+  },
   {
     field: "dateOfBirth",
     headerName: "DOB",
@@ -21,12 +36,54 @@ const columns = [
       });
     },
   },
-  { field: "gender", headerName: "Gender", width: 100 },
-  { field: "stateOfOrigin", headerName: "State of Origin", width: 160 },
-  { field: "lga", headerName: "LGA", width: 120 },
-  { field: "poolOffice", headerName: "Pool Office", width: 160 },
-  { field: "currentMDA", headerName: "Current MDA", width: 180 },
-  { field: "cadre", headerName: "Cadre", width: 120 },
+  {
+    field: "gender",
+    headerName: "Gender",
+    width: 100,
+    renderCell: (params) => (
+      <span>{params.value?.toUpperCase()}</span> // full uppercase
+    ),
+  },
+  {
+    field: "stateOfOrigin",
+    headerName: "State of Origin",
+    width: 160,
+    renderCell: (params) => (
+      <span>{params.value?.toUpperCase()}</span> // full uppercase
+    ),
+  },
+  {
+    field: "lga",
+    headerName: "LGA",
+    width: 120,
+    renderCell: (params) => (
+      <span>{params.value?.toUpperCase()}</span> // full uppercase
+    ),
+  },
+  {
+    field: "poolOffice",
+    headerName: "Pool Office",
+    width: 160,
+    renderCell: (params) => (
+      <span>{params.value?.toUpperCase()}</span> // full uppercase
+    ),
+  },
+  {
+    field: "currentMDA",
+    headerName: "Current MDA",
+    width: 180,
+    renderCell: (params) => (
+      <span>{params.value?.toUpperCase()}</span> // full uppercase
+    ),
+  },
+  {
+    field: "cadre",
+    headerName: "Cadre",
+    width: 120,
+    renderCell: (params) => (
+      <span>{params.value?.toUpperCase()}</span> // full uppercase
+    ),
+  },
   { field: "gradeLevel", headerName: "Grade Level", width: 140 },
   {
     field: "dateOfFirstAppointment",
@@ -75,7 +132,14 @@ const columns = [
   },
   { field: "phoneNumber", headerName: "Phone", width: 140 },
   { field: "email", headerName: "Email", width: 200 },
-  { field: "stateOfCurrentPosting", headerName: "Posting State", width: 160 },
+  {
+    field: "stateOfCurrentPosting",
+    headerName: "Posting State",
+    width: 160,
+    renderCell: (params) => (
+      <span>{params.value?.toUpperCase()}</span> // full uppercase
+    ),
+  },
   { field: "year2021", headerName: "2021", width: 100 },
   { field: "year2022", headerName: "2022", width: 100 },
   { field: "year2023", headerName: "2023", width: 100 },
@@ -91,6 +155,7 @@ function ViewCandidates() {
     const { data } = await httpService("viewcandidates");
 
     if (data) {
+      console.log(data);
       setStudent(data);
     }
     setLoading(false);
@@ -107,7 +172,7 @@ function ViewCandidates() {
         </div>
         <div style={{ height: 600, width: "100%" }}>
           <DataGrid
-            getRowId={(row) => row.ippisNumber}
+            //getRowId={(row) => row.ippisNumber}
             rows={student}
             columns={columns}
             pageSize={20}

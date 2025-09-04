022a20d2661d29db6249ecec9bd74f754027e6a1
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2025-09-04 09:41:35 +0100

    made changes

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -2,23 +2,25 @@ import { useAppUser } from "../../contexts/AppUserContext";
 import HRDashboard from "../../components/HRDashboard";
 import AdminComponent from "../../components/AdminComponent";
 import PromotionComponent from "../../components/PromotionComponent";
+import LoadingPage from "../../components/LoadingPage";
 
 function AdminDashboard() {
   const { user } = useAppUser();
 
+  // Map specificRole to component
+  const roleComponents = {
+    admin: <AdminComponent />,
+    hr: <HRDashboard />,
+    promotion: <PromotionComponent />,
+  };
+
+  if (user.role !== "admin") {
+    return null; // or maybe show "Not Authorized"
+  }
+
   return (
     <div className="mb-5 ">
-      {/* This is strictly for admin */}
-      {user.role === "admin" && user.specificRole === "admin" && (
-        <AdminComponent />
-      )}
-      {/* This is strictly for admin */}
-      {user.role === "admin" && user.specificRole === "hr" && <HRDashboard />}
-
-      {/* This is strictly for promotion */}
-      {user.role === "admin" && user.specificRole === "promotion" && (
-        <PromotionComponent />
-      )}
+      {roleComponents[user.specificRole] || <LoadingPage />}
     </div>
   );
 }

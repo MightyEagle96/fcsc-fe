046e24ef2e3468316ad0a66ff32c7733f4012b8c
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2025-09-06 07:23:42 +0100

    made changes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,19 @@
 import { Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppUser } from "../contexts/AppUserContext";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
+  const navigate = useNavigate();
+  const { user } = useAppUser();
+
+  useEffect(() => {
+    if (user.role === "admin") {
+      navigate(`/admin/dashboard`);
+    } else {
+      navigate(`/`);
+    }
+  }, []);
   return (
     <div>
       <div

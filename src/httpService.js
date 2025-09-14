acc55496b76c6bddde6c15a62881f602f0097f73
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2025-09-14 16:08:27 +0100

    made changes

--- src/httpService.js
+++ src/httpService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://fcsc.onrender.com/api"
-    : //"https://promotion.fedcivilservice.gov.ng/api"
+    ? "https://promotion.fedcivilservice.gov.ng/api"
+    : //
       "http://localhost:3456/api";
 
 // process.env.NODE_ENV === "production"

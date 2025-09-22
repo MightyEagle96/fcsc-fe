7fe7f75e4e5a96aa9f2d869a0ad62158d49bfd79
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2025-09-22 10:56:19 +0100

    made changes

--- src/pages/admin/Rejections.jsx
+++ src/pages/admin/Rejections.jsx
@@ -1,14 +1,141 @@
 import { Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { httpService } from "../../httpService";
+import { DataGrid } from "@mui/x-data-grid";
+import { Badge } from "react-bootstrap";
 
 function Rejections() {
+  const [loading, setLoading] = useState(false);
+  const [rejections, setRejections] = useState([]);
+  const [paginationModel, setPaginationModel] = useState({
+    page: 0, // DataGrid uses 0-based index
+    pageSize: 50, // rows per page
+  });
+  const [totalRejections, setTotalRejections] = useState(0);
+  const getData = async () => {
+    setLoading(true);
+    const { data } = await httpService("admin/rejections", {
+      params: {
+        page: paginationModel.page + 1,
+        limit: paginationModel.pageSize,
+      },
+    });
+
+    if (data) {
+      console.log(data);
+      setRejections(data.rejections);
+      setTotalRejections(data.total);
+
+      // setStudent(data.candidates);
+      // setRowCount(data.total);
+    }
+    setLoading(false);
+  };
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "S/N",
+      width: 90,
+      sortable: false,
+      filterable: false,
+    },
+    {
+      field: "candidate",
+      headerName: "Candidate",
+      width: 300,
+      renderCell: (param) => (
+        <span className="text-capitalize">{param.value.fullName}</span> // full uppercase
+      ),
+    },
+    {
+      field: "reason",
+      headerName: "Reason",
+      width: 400,
+    },
+    {
+      field: "rejectedBy",
+      headerName: "Rejected by",
+      width: 300,
+      renderCell: (param) => (
+        <span className="text-capitalize">
+          {param.value.firstName} {param.value.lastName}
+        </span>
+      ),
+    },
+    {
+      field: "createdAt",
+      headerName: "Date Rejected",
+      width: 200,
+      renderCell: (param) => (
+        <span className="text-capitalize">
+          {new Date(param.value).toLocaleDateString("en-NG")},{" "}
+          {new Date(param.value).toLocaleTimeString()}
+        </span>
+      ),
+    },
+    {
+      field: "notifiedByEmail",
+      headerName: "Notified By Email",
+      width: 200,
+      renderCell: (param) => (
+        <span className="text-capitalize">
+          {param.value ? (
+            <Badge bg="success">Yes</Badge>
+          ) : (
+            <Badge bg="danger">No</Badge>
+          )}
+        </span>
+      ),
+    },
+    {
+      field: "notifiedBySms",
+      headerName: "Notified By Sms",
+      width: 200,
+      renderCell: (param) => (
+        <span className="text-capitalize">
+          {param.value ? (
+            <Badge bg="success">Yes</Badge>
+          ) : (
+            <Badge bg="danger">No</Badge>
+          )}
+        </span>
+      ),
+    },
+
+    // {
+    //   field: "recommendedBy",
+    //   headerName: "Recommended By",
+    //   width: 200,
+    // },
+    // {
+    //   field: "uploadedDocuments",
+    //   headerName: "Uploaded Documents",
+    //   width: 200,
+    // },
+  ];
+  useEffect(() => {
+    getData();
+  }, [paginationModel]);
   return (
     <div>
       <div className="mt-5 mb-5">
-        <div className="container">
+        <div className="container mb-5">
           <Typography variant="h4" fontWeight={700}>
             Rejections
           </Typography>
+        </div>
+        <div className="p-3">
+          <DataGrid
+            loading={loading}
+            rows={rejections}
+            rowCount={totalRejections}
+            columns={columns}
+            paginationMode="server"
+            paginationModel={paginationModel}
+            onPaginationModelChange={setPaginationModel}
+            pageSizeOptions={[50, 100]}
+          />
         </div>
       </div>
     </div>
--- src/pages/admin/ViewCandidates.jsx
+++ src/pages/admin/ViewCandidates.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { httpService } from "../../httpService";
-import { Button, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { Delete } from "@mui/icons-material";
+
 import Swal from "sweetalert2";
 import { toast } from "react-toastify";
 import { Badge } from "react-bootstrap";
@@ -18,152 +18,13 @@ const columns = [
     //renderCell: (params) => params.api.getRowIndex(params.id) + 1,
   },
   {
-    field: "status",
-    headerName: "Status",
+    field: "candidate",
+    headerName: "Candidate",
     width: 150,
     renderCell: (params) => (
-      <Badge
-        style={{ textTransform: "uppercase" }}
-        bg={switchColors(params.value)}
-      >
-        {params.value}
-      </Badge>
+      <span className="text-capitalize">{params.value.firstName}</span> // full uppercase
     ),
-  },
-  { field: "ippisNumber", headerName: "IPPIS Number", width: 150 },
-  {
-    field: "fullName",
-    headerName: "Full Name",
-    width: 200,
-    renderCell: (params) => (
-      <span className="text-capitalize">{params.value}</span> // full uppercase
-    ),
-  },
-  {
-    field: "dateOfBirth",
-    headerName: "DOB",
-    width: 120,
-    renderCell: (params) => {
-      if (!params.value) return "";
-      const date = new Date(params.value);
-      if (isNaN(date.getTime())) return ""; // fallback if invalid
-      return date.toLocaleDateString("en-NG", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
-    },
-  },
-  {
-    field: "gender",
-    headerName: "Gender",
-    width: 100,
-    renderCell: (params) => (
-      <span className="text-capitalize">{params.value}</span> // full uppercase
-    ),
-  },
-  {
-    field: "stateOfOrigin",
-    headerName: "State of Origin",
-    width: 160,
-    renderCell: (params) => (
-      <span className="text-capitalize">{params.value}</span> // full uppercase
-    ),
-  },
-  {
-    field: "lga",
-    headerName: "LGA",
-    width: 120,
-    renderCell: (params) => (
-      <span className="text-capitalize">{params.value}</span> // full uppercase
-    ),
-  },
-  {
-    field: "poolOffice",
-    headerName: "Pool Office",
-    width: 160,
-    renderCell: (params) => (
-      <span className="text-capitalize">{params.value}</span> // full uppercase
-    ),
-  },
-  {
-    field: "currentMDA",
-    headerName: "Current MDA",
-    width: 180,
-    renderCell: (params) => (
-      <span className="text-capitalize">{params.value}</span> // full uppercase
-    ),
-  },
-  {
-    field: "cadre",
-    headerName: "Cadre",
-    width: 120,
-    renderCell: (params) => (
-      <span className="text-capitalize">{params.value}</span> // full uppercase
-    ),
-  },
-  { field: "gradeLevel", headerName: "Grade Level", width: 140 },
-  {
-    field: "dateOfFirstAppointment",
-    headerName: "First Appointment",
-    width: 160,
-    renderCell: (params) => {
-      if (!params.value) return "";
-      const date = new Date(params.value);
-      if (isNaN(date.getTime())) return ""; // fallback if invalid
-      return date.toLocaleDateString("en-NG", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
-    },
-  },
-  {
-    field: "dateOfConfirmation",
-    headerName: "Confirmation",
-    width: 160,
-    renderCell: (params) => {
-      if (!params.value) return "";
-      const date = new Date(params.value);
-      if (isNaN(date.getTime())) return ""; // fallback if invalid
-      return date.toLocaleDateString("en-NG", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
-    },
-  },
-  {
-    field: "dateOfLastPromotion",
-    headerName: "Last Promotion",
-    width: 160,
-    renderCell: (params) => {
-      if (!params.value) return "";
-      const date = new Date(params.value);
-      if (isNaN(date.getTime())) return ""; // fallback if invalid
-      return date.toLocaleDateString("en-NG", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
-    },
-  },
-  { field: "phoneNumber", headerName: "Phone", width: 140 },
-  { field: "email", headerName: "Email", width: 200 },
-  {
-    field: "stateOfCurrentPosting",
-    headerName: "Posting State",
-    width: 160,
-    renderCell: (params) => (
-      <span className="text-capitalize">{params.value}</span> // full uppercase
-    ),
-  },
-  { field: "year2021", headerName: "2021", width: 100 },
-  { field: "year2022", headerName: "2022", width: 100 },
-  { field: "year2023", headerName: "2023", width: 100 },
-  { field: "year2024", headerName: "2024", width: 100 },
-  { field: "remark", headerName: "Remark", width: 200 },
-  //  { field: "defaultPassword", headerName: "Password", width: 200 },
+  }, //  { field: "defaultPassword", headerName: "Password", width: 200 },
 ];
 
 function ViewCandidates() {
@@ -233,18 +94,17 @@ function ViewCandidates() {
             </div>
           </div>
         </div>
-        <div style={{ height: 600, width: "100%" }}>
-          <DataGrid
-            loading={loading}
-            rows={student}
-            columns={columns}
-            rowCount={rowCount}
-            paginationMode="server"
-            paginationModel={paginationModel}
-            onPaginationModelChange={setPaginationModel}
-            pageSizeOptions={[50, 100]}
-          />
-        </div>
+
+        <DataGrid
+          loading={loading}
+          rows={student}
+          columns={columns}
+          rowCount={rowCount}
+          paginationMode="server"
+          paginationModel={paginationModel}
+          onPaginationModelChange={setPaginationModel}
+          pageSizeOptions={[50, 100]}
+        />
       </div>
     </div>
   );
